d2e9ec8bff1c07552f3a2115b7a16a630d67b6df
mikolaszko <mikosodz.lote@gmail.com>
2024-02-12 09:18:29 +0100

    removed lib from bin/dune and implemented

--- bin/main.ml
+++ bin/main.ml
@@ -1,10 +1,12 @@
-type mimeImageInfo = {byte_pattern : string array; mask : string array;}
+(* type mimeImageInfo = {byte_pattern : string array; mask : string array;}
 
 let mimeJPG = { byte_pattern = [|"FF"; "D8"; "FF"|]; mask = [|"FF"; "FF"; "FF"|]}
 let mimeGIF = { byte_pattern = [|"47"; "49"; "46"; "38"|]; mask = [|"FF"; "FF"; "FF"; "FF"|]}
 let mimePNG = { byte_pattern = [|"89"; "50"; "4E"; "47"|]; mask = [|"FF"; "FF"; "FF"; "FF"|]}
 let mimeWEBP = { byte_pattern = [|"52"; "49"; "46"; "46"|]; mask = [|"FF"; "FF"; "FF"; "FF"|]}
- 
+*)
 
 let () =
-    Printf.printf "%s\n" Sys.argv.(1)
+    match Sys.file_exists Sys.argv.(1) with
+    | false -> raise (Invalid_argument "File doesn't exist")
+    | true -> print_endline "it does exist"
