ddbb0ff48582b08ad6290b34b88f1e472f385d0f
mikolaszko <mikosodz.lote@gmail.com>
2024-02-26 17:37:11 +0100

    project is done

--- bin/main.ml
+++ bin/main.ml
@@ -1,14 +1,21 @@
 open Base
-(* type mimeImageInfo = {byte_pattern : string array; mask : string array;}
 
-let mimeJPG = { byte_pattern = [|"FF"; "D8"; "FF"|]; mask = [|"FF"; "FF"; "FF"|]}
-let mimeGIF = { byte_pattern = [|"47"; "49"; "46"; "38"|]; mask = [|"FF"; "FF"; "FF"; "FF"|]}
-let mimePNG = { byte_pattern = [|"89"; "50"; "4E"; "47"|]; mask = [|"FF"; "FF"; "FF"; "FF"|]}
-let mimeWEBP = { byte_pattern = [|"52"; "49"; "46"; "46"|]; mask = [|"FF"; "FF"; "FF"; "FF"|]}
-*)
-let read_file_byte file = Option.value_exn (In_channel.with_open_bin file In_channel.input_byte)
+let read_bytes filename =
+  let ic = Stdio.In_channel.create filename in 
+  let pattern_bytes = Bytes.create 4 in
+  Option.value_exn (Stdio.In_channel.really_input ic ~buf:pattern_bytes ~pos: 0 ~len:4);
+  Stdio.In_channel.close ic;
+  match Bytes.to_string pattern_bytes with 
+  | "\x25\x50\x44\x46" -> "application/pdf"
+  | "\x89\x50\x4E\x47" -> "image/png"
+  | "\x46\x49\x46\x36" -> "image/gif"
+  | "\xFF\xD8\xFF" -> "image/jpeg"
+  | _ -> "dont/care" (* Default MIME type *)
 
-let () = 
+;;
+
+let () =
   let array_of_argument = Sys.get_argv () in
-  let byte = read_file_byte array_of_argument.(1)  in
-  Stdlib.print_int byte
+  let filename = array_of_argument.(1)  in
+  let mime_type = read_bytes filename in
+  Stdlib.print_endline mime_type
